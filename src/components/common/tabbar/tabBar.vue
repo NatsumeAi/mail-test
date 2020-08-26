1c7e5bb88338c2185f51a506332608e7c7b7feae
Ai Natsume <2523002956@qq.com>
2020-08-26 10:44:06 +0800

    18.03.01

---
+++ src/components/common/tabbar/tabBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="tab-bar">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'tabBar',
+    components:{
+
+    }
+}
+</script>
+
+<style scoped>
+    #tab-bar{
+        display:flex;
+        background:#f2f2f2;
+        position:fixed;
+        bottom:0;
+        left:0;
+        right:0;
+        box-shadow: 0px -2px 2px rgba(100,100,100,0.1);
+    }
+</style>
---
+++ src/components/content/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <tabbar>
+        <tabbaritem></tabbaritem>
+        <tabbaritem></tabbaritem>
+        <tabbaritem></tabbaritem>
+        <tabbaritem></tabbaritem>
+    </tabbar>
+</template>
+
+<script>
+export default {
+    name :'App',
+    components:{
+        tabbar,
+        tabbaritem
+    }
+}
+
+import tabbaritem from '../common/tabbar/tabBarItem';
+import tabbar from '../common/tabbar/tabBar';
+</script>
+
+<style>
+</style>
